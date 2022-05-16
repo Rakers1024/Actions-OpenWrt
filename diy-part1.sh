8cb7b13ce27a06d71ceef46349ae879ea910bd44
卓志诚 <1139991371@qq.com>
2022-05-16 22:48:16 +0800

    使用lean脚本

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
+#=============================================================
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# fw876/helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# Lienol/openwrt-package
+# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/linkease/istore.git && mv istore/luci/luci-app-store package/ && rm -rf istore
-git clone https://github.com/lisaac/luci-lib-docker.git && mv luci-lib-docker/collections/luci-lib-docker package/ && rm -rf luci-lib-docker
-git clone https://github.com/lisaac/luci-app-dockerman.git && mv luci-app-dockerman/applications/luci-app-dockerman package/ && rm -rf luci-app-dockerman
-git clone https://github.com/sirpdboy/netspeedtest.git && mv netspeedtest/luci-app-netspeedtest package/ && rm -rf netspeedtest
