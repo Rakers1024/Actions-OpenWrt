d8586ee2385fd9cd72aef43680da971778a477fe
卓志诚 <1139991371@qq.com>
2022-05-16 16:25:01 +0800

    尝试修复默认主题

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,12 +11,10 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 替换主题
 # 删除自定义源默认的 argon 主题
 rm -rf package/lean/luci-theme-argon
-# 拉取 argon 原作者的源码
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
